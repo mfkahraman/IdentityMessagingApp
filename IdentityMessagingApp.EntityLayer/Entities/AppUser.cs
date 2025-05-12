0b81626763755a07d3d5a514e0bd0c78c4af8808
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-05-12 18:27:51 +0300

    Add project files.

---
+++ IdentityMessagingApp.EntityLayer/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityMessagingApp.EntityLayer.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        // Gönderdiği mesajlar
+        public ICollection<Messagesda> SentMessages { get; set; }
+
+        // Aldığı mesajlar
+        public ICollection<Messagesda> ReceivedMessages { get; set; }
+    }
+}
---
+++ IdentityMessagingApp.EntityLayer/Entities/Message.cs
@@ -0,0 +1,30 @@
+﻿using IdentityMessagingApp.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityMessagingApp.EntityLayer.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string SenderId { get; set; }
+        public AppUser Sender { get; set; }
+
+        public string ReceiverId { get; set; }
+        public AppUser Receiver { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Content { get; set; } // Rich Text HTML
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public bool IsDraft { get; set; }
+        public bool IsDeletedBySender { get; set; }
+        public bool IsDeletedByReceiver { get; set; }
+    }
+}
